4f3e9e19db4e17d90c5c42f205419dd4d2f4738b
Michael Garrison <mgarrison.sa@gmail.com>
2020-05-18 21:20:29 -0500

    completed practice assessment

---
+++ js/edabit-exercises.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// create a function that takes two numbers as arguments and return their sum
+function addtion (a,b) {
+    return a + b;
+}
+
+// Create a function that takes height and width and finds the perimeter of a rectangle
